c0df5764028b1ab569e4c66241d429dc35989eac
BA-24 <65275759+BA-24@users.noreply.github.com>
2020-05-27 11:32:12 +0200

    topicDelete

---
+++ tDel.py
@@ -0,0 +1,15 @@
+def tDel(number):
+    try:
+        f = open('topics.txt', 'r')
+        topics = f.readlines()
+        f.close()
+        if number >= len(topics):
+            return [True, None]
+        else:
+            topics.remove(topics[number])
+            f = open('topics.txt', 'w')
+            f.write(''.join(topics))
+            f.close()
+            return [True, None]
+    except Exception as error:
+        return [False, 'Error in tDel: ' + str(error)]
