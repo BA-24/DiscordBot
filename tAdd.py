f11f4b98b70fb63803d6dd3d6fc018cd73c106ec
BA-24 <65275759+BA-24@users.noreply.github.com>
2020-05-29 08:59:55 +0200

    moved to file management folder

--- tAdd.py
+++
@@ -1,8 +0,0 @@
-def tAdd(msg):
-    try:
-        x = open('topics.txt', 'a')
-        x.write(msg)
-        x.close()
-        return [True, None]
-    except Exception as error:
-        return [False, 'Error in tAdd: ' + str(error)]
