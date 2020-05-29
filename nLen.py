cd848f03ba76fe2771c0f03019326853c3181441
BA-24 <65275759+BA-24@users.noreply.github.com>
2020-05-29 08:59:45 +0200

    moved to file management folder

--- nLen.py
+++
@@ -1,9 +0,0 @@
-def nLen():
-    try:
-        x = open('notes.txt', 'r')
-        notes = x.readlines()
-        x.close()
-        return [True, len(notes)]
-    
-    except Exception as error:
-        return [False, 'Error in nLen: ' + str(error)]
