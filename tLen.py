d65bc32178140f83ed71b2db90cff57bde49af3f
BA-24 <65275759+BA-24@users.noreply.github.com>
2020-05-29 09:00:30 +0200

    moved to file management folder

--- tLen.py
+++
@@ -1,8 +0,0 @@
-def tLen():
-    try:
-        f = open('topics.txt', 'r')
-        topics = f.readlines()
-        f.close()
-        return [True, len(topics)]
-    except Exception as error:
-        return [False, 'Error in tLen: ' + str(error)]
