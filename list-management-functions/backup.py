1c89495351184af3852fe88b84efe38cb2c5c809
BA-24 <65275759+BA-24@users.noreply.github.com>
2020-06-01 19:52:23 +0200

    function probably doesn't work

---
+++ list-management-functions/backup.py
@@ -0,0 +1,36 @@
+#THIS FUNCTION HAS NOT BEEN TESTED AND PROBABLY DOESN'T WORK
+
+def backup(mode):
+    try:
+        if mode == 'save':
+            f = open('notes.txt', 'r')
+            notes = f.read()
+            f.close()
+            f = open('topics.txt', 'r')
+            topics = f.read()
+            f.close()
+            f = open('backup\notes.txt', 'w')
+            f.write(notes)
+            f.close()
+            f = open('backup\topics.txt', 'w')
+            f.write(topics)
+            f.close()
+            return [True, 'save']
+        elif mode == 'load':
+            f = open('backup\notes.txt', 'r')
+            notes = f.read()
+            f.close()
+            f = open('backup\topics.txt', 'r')
+            topics = f.read()
+            f.close()
+            f = open('notes.txt', 'w')
+            f.write(notes)
+            f.close()
+            f = open('topics.txt', 'w')
+            f.write(topics)
+            f.close()
+            return [True, 'load']
+        return [True, None]
+        
+    except Exception as error:
+        return[False, error]
